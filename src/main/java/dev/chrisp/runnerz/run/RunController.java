1ec439eb113e5624f7a886ef7e41bdb60e62dbf6
Christopher Purkiss <christopher.purkiss@students.mq.edu.au>
2025-01-22 18:08:46 +1100

    adding get endpoint

---
+++ src/main/java/dev/chrisp/runnerz/run/RunController.java
@@ -0,0 +1,22 @@
+package dev.chrisp.runnerz.run;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class RunController {
+
+    private final RunRepository runRepository;
+
+    public RunController(RunRepository runRepository){
+        this.runRepository = runRepository;
+    }
+
+    @GetMapping("/api/runs")
+    List<Run> findAll() {
+        return runRepository.findAll();
+    }
+}
---
+++ src/main/java/dev/chrisp/runnerz/run/RunRepository.java
@@ -0,0 +1,37 @@
+package dev.chrisp.runnerz.run;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class RunRepository {
+
+    private List<Run> runs = new ArrayList<>();
+
+    List<Run> findAll(){
+        return runs;
+    }
+
+    @PostConstruct
+    private void init(){
+        runs.add(new Run(
+                1,
+                "Monday Morning Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plusHours(1),
+                3,
+                Location.INDOOR));
+
+        runs.add(new Run(
+                2,
+                "Monday Afternoon Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plusHours(2),
+                20,
+                Location.OUTDOOR));
+    }
+}
